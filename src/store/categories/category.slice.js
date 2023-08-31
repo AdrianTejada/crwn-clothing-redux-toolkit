923a1cf3ba981f689aff041f91de89c0bd7bb2a9
AdrianTejada <atejada@my.bcit.ca>
2023-08-30 20:04:38 -0700

    added thunk to categories

--- src/store/categories/category.slice.js
+++ src/store/categories/category.slice.js
@@ -1,20 +1,41 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
 
 const CATEGORIES_INITIAL_STATE = {
   categories: [],
+  isLoading: false,
+  error: null
 };
 
+export const getCategoriesMap = createAsyncThunk(
+  'category/getCategoriesMap',
+  async () => {
+    return await getCategoriesAndDocuments('categories')
+  }
+)
 
 const categoriesSlice = createSlice({
   name:'category',
   initialState: CATEGORIES_INITIAL_STATE,
-  reducers: {
-    setCategories(state, action) {
+  extraReducers: builder => {
+    builder.addCase(getCategoriesMap.pending, (state) => {
+      state.isLoading = true;
+    })
+    builder.addCase(getCategoriesMap.fulfilled, (state, action) => {
+      state.isLoading = false;
       state.categories = action.payload;
-    }
+      state.error = null;
+    })
+    builder.addCase(getCategoriesMap.rejected, (state, action) => {
+      state.isLoading = false; 
+      state.categories = [];
+      state.error = action.error.message;
+    })
   }
 });
 
 
-export const {setCategories} = categoriesSlice.actions;
+
 export const categoriesReducer = categoriesSlice.reducer;
